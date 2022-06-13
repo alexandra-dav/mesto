825fe9250e98e224d6b6b4bc1cbbced76ca3deda
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-06-13 18:29:44 +0300

    added const initialCards in js-file

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,35 @@ let popupName = document.querySelector('.popup__text_form_name');
 let popupJob = document.querySelector('.popup__text_form_job');
 let formElement = document.querySelector('form[name="popup-form"]');
 
+/* При загрузке на странице должно быть 6 карточек, 
+которые добавит JavaScript.  */
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
 function OpenPopup() {
     popupW.classList.add('popup_opened');
     /* Подтягивание значений полей в попап при открытии */
