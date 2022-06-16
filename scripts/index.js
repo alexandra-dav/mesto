0f9c631d77f0323890cbe9634a67e47eb9d30f2c
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-06-16 20:19:20 +0300

    add new elements

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,9 @@ let popupWProfile = document.querySelector('#edit_profile');
 let popupWAddElements = document.querySelector('#add_elements');
 let popupName = document.querySelector('.popup__text_form_name');
 let popupJob = document.querySelector('.popup__text_form_job');
-let formElement = document.querySelector('form[name="popup-form"]');
+let formElement1 = document.querySelector('form[name="popup-form1"]');
+let formElement2 = document.querySelector('form[name="popup-form2"]');
+let ElementsContainer = document.querySelector('.elements');
 
 /* При загрузке на странице должно быть 6 карточек, 
 которые добавит JavaScript.  */
@@ -37,22 +39,6 @@ const initialCards = [
     }
   ];
 
-// Функция, которая при обновлении страницы добавляет карточки
-function initialFirstCards() {
-  const SectionElements = document.querySelector('.elements');
-  for(let i = 0; i < initialCards.length; i++){
-    SectionElements.insertAdjacentHTML('beforeend', `
-    <article class="elements__container">
-      <img src="${initialCards[i].link}" alt="${initialCards[i].name}" class="elements__image">
-      <div class="elements__info">
-        <h2 class="elements__name">${initialCards[i].name}</h2>
-        <button aria-label="like" name="favorit" id="favorit${i}" class="elements__favorit" type="button"></button>
-      </div>
-    </article>
-    `);
-  }
-}
-
 function OpenPopup(thisPopup) {
     thisPopup.classList.add('popup_opened');
     /* Подтягивание значений полей в попап при открытии */
@@ -68,8 +54,8 @@ function ClosePopup(thisPopup) {
 
 /* Переключатель лайков */
 function favorits () {
-    var header = document.getElementById("elements");
-    var btns = header.getElementsByClassName("elements__favorit");
+  const header = document.getElementById("elements");
+  const btns = header.getElementsByClassName("elements__favorit");
     for (var i = 0; i < btns.length; i++) {
         btns[i].addEventListener("click", function() {
             if(this.className == "elements__favorit"){
@@ -80,23 +66,52 @@ function favorits () {
 }
 
 /* Работа с изменением данных формы */
+function MakeElementPlase (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Берем данные из попапа
+  const PlaceName = document.querySelector('.popup__text_form_plase').value;
+  const PlaceLink = document.querySelector('.popup__text_form_link').value;
+
+  addElementPlase(PlaceName, PlaceLink);
+}
+
+function addElementPlase (name, link) {
+  const plaseTemplate = document.querySelector('#plase-template').content;
+  const ElementContainer = plaseTemplate.querySelector('.elements__container').cloneNode(true);
+
+  ElementContainer.querySelector('.elements__name').textContent = name;
+  ElementContainer.querySelector('.elements__image').src = link;
+  ElementContainer.querySelector('.elements__image').alt = name;
+  ElementContainer.querySelector('.elements__favorit').id = `favorit${Date.now()}`;
+
+  ElementsContainer.prepend(ElementContainer);
+  ClosePopup(popupWAddElements);
+  favorits();
+}
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Получите значение полей jobInput и nameInput из свойства value
-    var jobInput = popupJob.value;
-    var nameInput = popupName.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-    var docName = document.querySelector('.profile__name');
-    var docJob = document.querySelector('.profile__occupation');
-    // Вставьте новые значения с помощью textContent
-    docName.textContent = nameInput;
-    docJob.textContent = jobInput;
-    ClosePopup();
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Получите значение полей jobInput и nameInput из свойства value
+  const jobInput = popupJob.value;
+  const nameInput = popupName.value;
+  // Выберите элементы, куда должны быть вставлены значения полей
+  const docName = document.querySelector('.profile__name');
+  const docJob = document.querySelector('.profile__occupation');
+  // Вставьте новые значения с помощью textContent
+  docName.textContent = nameInput;
+  docJob.textContent = jobInput;
+  ClosePopup(popupWProfile);
+}
+
+// Создаем дефолтное наполнение
+for(let i = 0; i < initialCards.length; i++) {
+  addElementPlase(initialCards[i].name, initialCards[i].link);
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement1.addEventListener('submit', formSubmitHandler);
+formElement2.addEventListener('submit', MakeElementPlase);
 
 /* Добавление модификатора при открытии попапа */
 buttonEdit.addEventListener('click', () => {OpenPopup(popupWProfile)});
@@ -123,9 +138,3 @@ buttonCloseAddElements.addEventListener('click', () => {ClosePopup(popupWAddElem
         popupW.classList.remove('popup_opened');
     }
 }); */
-
-// Вызов функции дефолтного автозаполнения карточек
-initialFirstCards();
-
-/* Вызов функции с лайками */
-favorits();
