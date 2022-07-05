a37b998034db49381b3def799d286f36ee332fb2
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-07-05 09:01:08 +0300

    validate form

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,78 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__error_visible');
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__error_visible');
+    errorElement.textContent = '';
+  };
+
+
+
+// Проверка инпутов при вводе
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// Проверка валидности формы
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+      // Если поле не валидно, колбэк вернёт true
+      // Обход массива прекратится и вся функция
+      // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  });
+};
+
+// Переключатель классов: зависит от тог валидна ли форма
+const toggleButtonState = (inputList, buttonElement) => { //Первый — массив полей, второй — кнопка «Далее».
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button_disabled');
+  } else {
+    buttonElement.classList.remove('popup__button_disabled');
+  }
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    }); 
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input')); // находим все инпуты
+    const buttonElement = formElement.querySelector('.popup__button'); // находим кнопку
+    toggleButtonState(inputList, buttonElement); // проверяем изначально форма валидна?
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  });
+};
+
+
+
+
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
-enableValidation({
+/* enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible'
-  });
+  }); */
+
+  enableValidation();
