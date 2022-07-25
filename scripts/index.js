11189c84af7a72f3d29fd663a7acf8da534e270f
Alexandra Davydova <davydovaalexandra225@gmail.com>
2022-07-25 20:56:27 +0300

    added new file data

--- scripts/index.js
+++ scripts/index.js
@@ -27,45 +27,6 @@ const buttonClosePhoto = popupWPhoto.querySelector('.popup__close_window_photo')
 
 const buttonElement = popupWAddElements.querySelector('.popup__button'); // находим кнопку
 
-/* При загрузке на странице должно быть 6 карточек, 
-которые добавит JavaScript.  */
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
-// Массив с классами ошибок
-  const errorList = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-  };
-
 //Закрыть попап на нажатию Escape
 const clickEscape = (e) => {
   const key = e.code;
